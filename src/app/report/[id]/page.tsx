e7b3be3c830903df5add0da895ebdf39343c5935
abdullah-gashia <s6630613012@phuket.psu.ac.th>
2024-10-16 23:29:01 +0700

    update

--- src/app/report/[id]/page.tsx
+++ src/app/report/[id]/page.tsx
@@ -1,19 +1,20 @@
-import CC from "@/components/cc";
-
 export default function Params(
-    { params, searchParams }:
-        {
-            params: { slug: string },
-            searchParams: { [key: string]: string | string[] | undefined }
-        }) {
-    return (<>
-        Params: {params.slug}
-        <hr />
-        CC: <CC />
-        <hr />
-        QueryString: <br />
-        SearchParams: {JSON.stringify(searchParams)}
-        <br />
-        Guitar: {searchParams.guitar}
-    </>)
+  { params, searchParams }:
+      {
+          params: { slug: string },
+          searchParams: { [key: string]: string | string[] | undefined }
+      }) {
+  return (
+      <>
+          <p>Params: {params.slug}</p>
+          <p>QueryString:</p>
+          <p>SearchParams: {JSON.stringify(searchParams)}</p>
+          <p>Guitar: {Array.isArray(searchParams.guitar) ? searchParams.guitar.join(', ') : searchParams.guitar}</p>
+          {searchParams.guitar && typeof searchParams.guitar === 'string' && (
+              <div>
+                  <img src={searchParams.guitar} alt="Guitar" />
+              </div>
+          )}
+      </>
+  );
 }
